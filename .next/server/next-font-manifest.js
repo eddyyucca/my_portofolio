c9d3bb25492f9fc295581c418b200ec7cc7c0190
eddyyucca <eddyyucca@gmail.com>
2025-02-26 00:58:54 +0800

    update

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Applications/MAMP/htdocs/my_portofolio/src/app/layout\":[\"static/media/a34f9d1faa5f3315-s.p.woff2\",\"static/media/92f44bb82993d879-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
